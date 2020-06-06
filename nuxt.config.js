0b1b939821e9eb0ca8b3eb26f24d4c4d2524a0b2
ezhang7423 <ezhang7423@student.palomar.edu>
2020-06-06 00:06:45 -0700

    ci is crazy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,10 @@
 import colors from "vuetify/es5/util/colors";
-const routerBase =
-  process.env.DEPLOY_ENV === "GH_PAGES"
-    ? {
-        router: {
-          base: "https://github.com/ezhang7423/vuegrades"
-        }
-      }
-    : {};
+
 export default {
   router: {
     base:
       process.env.DEPLOY_ENV === "GH_PAGES"
-        ? "https://github.com/ezhang7423/vuegrades"
+        ? "https://ezhang7423.github.io/vuegrades/"
         : "/"
   },
 
