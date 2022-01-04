c06c296315fd72d7a04f3120a1fcf62ed7465472
ezhang7423 <ezhang7423@student.palomar.edu>
2022-01-04 15:04:36 -0800

    Updates

--- _nuxt/static/1641337087/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641337087",layout:"default",error:b,state:{advanced:{name:a,editmode:false},classes:[],comcom:{old:a,new:a,com:a,comcom:a,course:a,type:a},name:{name:a}},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:b}}}}("",null,"\u002F"));
